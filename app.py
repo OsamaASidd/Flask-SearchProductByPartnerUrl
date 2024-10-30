0254f4c30f4ac6ba9609ba5871c508b68e0744f9
OsamaASidd <k200216@nu.edu.pk>
2024-10-30 12:41:07 +0500

    first commit

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ import json
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
-from google.cloud import language
 from flask import Flask, request, jsonify
-
+from algoliasearch.search_client import SearchClient
 app = Flask(__name__)
+
+# Initialize Algolia client
+ALGOLIA_APP_ID = 'EMFUSB67CG'
+ALGOLIA_API_KEY = '0f7466afc30b24c667a61b43cf879898'
+ALGOLIA_INDEX_NAME = 'products'
+
+client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
+index = client.init_index(ALGOLIA_INDEX_NAME)
+
 
 def get_firebase_credentials():
     # Use environment variables for sensitive information
@@ -22,10 +30,6 @@ try:
     cred = get_firebase_credentials()
     firebase_admin.initialize_app(cred)
     db = firestore.client()
-
-    # Initialize the Google Cloud client with the same credentials if possible
-    # client = language_v1.LanguageServiceClient(credentials=cred)
-    client = language.LanguageServiceClient(credentials=cred)
 except Exception as e:
     print(f"Error initializing services: {e}")
     raise
@@ -35,29 +39,32 @@ def hello() -> str:
     """Return a friendly HTTP greeting."""
     return "Hello World!"
 
-@app.route('/getTextEvaluation/<text>', methods=['GET'])
-def text_evaluation(text):
-   
-    if not text:
-        return jsonify({"error": "No text provided"}), 400
-    print(text)
-    try:
-        # Prepare the document with the input text
-        document = language.Document(
-        content=text,
-        type_=language.Document.Type.PLAIN_TEXT,
-        )
-        print(client.moderate_text(document=document))
-        # Detects the sentiment of the text
-        # sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+@app.route('/searchProduct', methods=['GET'])
+async def search_product():
+        try:
+        # Get query parameters from request
+            query = request.args.get('query', '')
+            page = int(request.args.get('page', 0))  # Get page number (default 0)
+            hits_per_page = int(request.args.get('hitsPerPage', 1))  # Default 9 results per page
 
-        # Format the response data
-        # # print(f"Text: {text}")
-        # print(f"Sentiment: {sentiment.score}, {sentiment.magnitude}")
-        # result = {'score': sentiment.score, 'magnitude': sentiment.magnitude}
-        # return jsonify(result), 200
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+            if len(query) < 3:
+                return jsonify({"error": "Search query must be at least 3 characters"}), 400
+
+            search_result =  index.search(query, {
+                'attributesToRetrieve': [
+                    'objectID',
+                    'name',
+                    'subSubCategory',
+                    'imageUrl'
+                ],
+                'hitsPerPage': hits_per_page
+            })
+
+            return jsonify(search_result), 200
+
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
